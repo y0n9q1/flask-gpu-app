41925e348d156c17de0a14657f3f716f7bdce5d0
y0n9q1 <48194197+y0n9q1@users.noreply.github.com>
2023-06-01 14:43:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import base64
 from io import BytesIO
 
 # Load model
-pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", revision="fp16", torch_dtype=torch.float16)
+# pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", revision="fp16", torch_dtype=torch.float16)
+pipe = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1", revision="fp16", torch_dtype=torch.float16)
 pipe.to("cuda")
 
 # Start flask app and set to ngrok
